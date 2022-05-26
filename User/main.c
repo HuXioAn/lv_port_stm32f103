499a2e5ce1141ed55ae456ab84bf8f2013cf0967
胡小安 <hu7006@outlook.com>
2022-05-26 13:47:11 +0800

    添加屏幕与触控部分的上层驱动

--- User/main.c
+++ User/main.c
@@ -1,40 +1,23 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  fire
-  * @version V1.0
-  * @date    2013-xx-xx
-  * @brief   测试led
-  ******************************************************************************
-  * @attention
-  *
-  * 实验平台:野火 F103 STM32 开发板 
-  * 论坛    :http://www.firebbs.cn
-  * 淘宝    :https://fire-stm32.taobao.com
-  *
-  ******************************************************************************
-  */ 
 
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f1xx.h"
 #include "./usart/bsp_debug_usart.h"
-#include "./led/bsp_led.h"
 #include <stdio.h>
 #include "./lcd/bsp_ili9341_lcd.h"
+#include "lvgl.h"
 
-static void LCD_Test(void);	
-static void Delay ( __IO uint32_t nCount );
-void Printf_Charater(void)   ;
+#define LCD_VERTICAL_RES 240
+#define LCD_HORIZONTAL_RES 320
 
-extern uint16_t lcdid;
+#define BUFFER_WIDTH 24
 
-/*
- * 函数名：main
- * 描述  ：主函数
- * 输入  ：无
- * 输出  ：无
- */
+static lv_disp_draw_buf_t disp_buf;
+static lv_disp_drv_t      disp_drv;
+
+static lv_color_t buf_1[LCD_HORIZONTAL_RES * BUFFER_WIDTH];
+static lv_color_t buf_2[LCD_HORIZONTAL_RES * BUFFER_WIDTH];
+
+
 int main(void)
 {   
   /* 设定系统时钟为72MHz */
@@ -43,141 +26,28 @@ int main(void)
 	/* 配置串口1为：115200 8-N-1 */
 	DEBUG_USART_Config();
   
-	ILI9341_Init ();         //LCD 初始化
+  lv_init();
+  ILI9341_Init ();         //LCD 初始化
 
-	printf("\r\n ********** 液晶屏英文显示程序*********** \r\n"); 
-	printf("\r\n 本程序不支持中文，显示中文的程序请学习下一章 \r\n"); 
-	
- //其中0、3、5、6 模式适合从左至右显示文字，
- //不推荐使用其它模式显示文字	其它模式显示文字会有镜像效果			
- //其中 6 模式为大部分液晶例程的默认显示方向  
-  if(lcdid == LCDID_ILI9341)
-  {
-    ILI9341_GramScan ( 6 );
-  }
-  else if(lcdid == LCDID_ST7789V)
-  {
-    ILI9341_GramScan ( 0 );
-  }
-  
-	while ( 1 )
-	{
-		LCD_Test();
-	}		
-}
+  //初始化buffer
+	lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, LCD_HORIZONTAL_RES*BUFFER_WIDTH);
+  //初始化显示驱动
+  lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
+  disp_drv.draw_buf = &disp_buf;          /*Set an initialized buffer*/
+  disp_drv.flush_cb = my_flush_cb;        /*Set a flush callback to draw to the display*/
+  disp_drv.hor_res = LCD_HORIZONTAL_RES;                 /*Set the horizontal resolution in pixels*/
+  disp_drv.ver_res = LCD_VERTICAL_RES;                 /*Set the vertical resolution in pixels*/
+
+  lv_disp_t * disp;
+  disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
 
 
-/*用于测试各种液晶的函数*/
-void LCD_Test(void)
-{
-	/*演示显示变量*/
-	static uint8_t testCNT = 0;	
-	char dispBuff[100];
-	
-	testCNT++;	
-	
-	LCD_SetFont(&Font8x16);
-	LCD_SetColors(RED,BLACK);
-
-  ILI9341_Clear(0,0,LCD_X_LENGTH,LCD_Y_LENGTH);	/* 清屏，显示全黑 */
-	/********显示字符串示例*******/
-  ILI9341_DispStringLine_EN(LINE(0),"BH 3.2_2.8 inch LCD para:");
-  ILI9341_DispStringLine_EN(LINE(1),"Image resolution:240x320 px");
-  if(lcdid == LCDID_ILI9341)
-  {
-    ILI9341_DispStringLine_EN(LINE(2),"ILI9341 LCD driver");
-  }
-  else if(lcdid == LCDID_ST7789V)
-  {
-    ILI9341_DispStringLine_EN(LINE(2),"ST7789V LCD driver");
-  }
-  ILI9341_DispStringLine_EN(LINE(3),"XPT2046 Touch Pad driver");
-  
-	/********显示变量示例*******/
-	LCD_SetFont(&Font16x24);
-	LCD_SetTextColor(GREEN);
-
-	/*使用c标准库把变量转化成字符串*/
-	sprintf(dispBuff,"Count : %d ",testCNT);
-  LCD_ClearLine(LINE(4));	/* 清除单行文字 */
-	
-	/*然后显示该字符串即可，其它变量也是这样处理*/
-	ILI9341_DispStringLine_EN(LINE(4),dispBuff);
-
-	/*******显示图形示例******/
-	LCD_SetFont(&Font24x32);
-  /* 画直线 */
-  
-  LCD_ClearLine(LINE(4));/* 清除单行文字 */
-	LCD_SetTextColor(BLUE);
-
-  ILI9341_DispStringLine_EN(LINE(4),"Draw line:");
-  
-	LCD_SetTextColor(RED);
-  ILI9341_DrawLine(50,170,210,230);  
-  ILI9341_DrawLine(50,200,210,240);
-  
-	LCD_SetTextColor(GREEN);
-  ILI9341_DrawLine(100,170,200,230);  
-  ILI9341_DrawLine(200,200,220,240);
-	
-	LCD_SetTextColor(BLUE);
-  ILI9341_DrawLine(110,170,110,230);  
-  ILI9341_DrawLine(130,200,220,240);
-  
-  Delay(0xFFFFFF);
-  
-  ILI9341_Clear(0,16*8,LCD_X_LENGTH,LCD_Y_LENGTH-16*8);	/* 清屏，显示全黑 */
-  
-  
-  /*画矩形*/
-
-  LCD_ClearLine(LINE(4));	/* 清除单行文字 */
-	LCD_SetTextColor(BLUE);
-
-  ILI9341_DispStringLine_EN(LINE(4),"Draw Rect:");
-
-	LCD_SetTextColor(RED);
-  ILI9341_DrawRectangle(50,200,100,30,1);
-	
-	LCD_SetTextColor(GREEN);
-  ILI9341_DrawRectangle(160,200,20,40,0);
-	
-	LCD_SetTextColor(BLUE);
-  ILI9341_DrawRectangle(170,200,50,20,1);
-  
-  
-  Delay(0xFFFFFF);
-	
-	ILI9341_Clear(0,16*8,LCD_X_LENGTH,LCD_Y_LENGTH-16*8);	/* 清屏，显示全黑 */
-
-  /* 画圆 */
-  LCD_ClearLine(LINE(4));	/* 清除单行文字 */
-	LCD_SetTextColor(BLUE);
-	
-  ILI9341_DispStringLine_EN(LINE(4),"Draw Cir:");
-
-	LCD_SetTextColor(RED);
-  ILI9341_DrawCircle(100,200,20,0);
-	
-	LCD_SetTextColor(GREEN);
-  ILI9341_DrawCircle(100,200,10,1);
-	
-	LCD_SetTextColor(BLUE);
-	ILI9341_DrawCircle(140,200,20,0);
-
-  Delay(0xFFFFFF);
-  
-  ILI9341_Clear(0,16*8,LCD_X_LENGTH,LCD_Y_LENGTH-16*8);	/* 清屏，显示全黑 */
 
 }
 
 
-/**
-  * @brief  简单延时函数
-  * @param  nCount ：延时计数值
-  * @retval 无
-  */	
+
+
 static void Delay ( __IO uint32_t nCount )
 {
   for ( ; nCount != 0; nCount -- );
@@ -233,4 +103,3 @@ void SystemClock_Config(void)
   }
 }
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
